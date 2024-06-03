bdd19499fbfca32091d87f160e654c82cd6951d2
Erik5CA <erik.castillo.anzures@gmail.com>
2024-06-02 21:34:09 -0600

    finish exercises 1.6 to 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,19 +4,55 @@ export const Button = ({ handleClick, text }) => {
   return <button onClick={handleClick}>{text}</button>;
 };
 
+export const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  );
+};
+
+export const Statistics = ({ good, bad, neutral, average }) => {
+  if (good === 0 && bad === 0 && neutral === 0) {
+    return <p>No feedback given</p>;
+  }
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text={"good"} value={good} />
+        <StatisticLine text={"neutral"} value={neutral} />
+        <StatisticLine text={"bad"} value={bad} />
+        <StatisticLine text={"all"} value={good + neutral + bad} />
+        <StatisticLine
+          text={"average"}
+          value={average / (good + neutral + bad)}
+        />
+        <StatisticLine
+          text={"positive"}
+          value={(good * 100) / (good + neutral + bad)}
+        />
+      </tbody>
+    </table>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [average, setAverage] = useState(0);
 
   const onGood = () => {
     setGood(good + 1);
+    setAverage(average + 1);
   };
   const onNeutral = () => {
     setNeutral(neutral + 1);
   };
   const onBad = () => {
     setBad(bad + 1);
+    setAverage(average - 1);
   };
 
   return (
@@ -27,9 +63,7 @@ const App = () => {
       <Button text="bad" handleClick={onBad} />
 
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} average={average} />
     </div>
   );
 };
