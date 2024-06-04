9c73a6d94ee3a232dee0a7153d1e1be3419a6ab6
Erik5CA <erik.castillo.anzures@gmail.com>
2024-06-04 13:23:08 -0600

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const Filter = ({ handleFilter }) => {
   return (
@@ -45,15 +46,17 @@ export const Persons = ({ personFil }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [personFil, setPersonFil] = useState(persons);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then((response) => {
+      setPersons(response.data);
+      setPersonFil(response.data);
+    });
+  }, []);
 
   const handleNewName = (event) => {
     setNewName(event.target.value);
